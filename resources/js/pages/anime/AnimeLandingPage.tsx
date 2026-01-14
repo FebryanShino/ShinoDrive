3e544ababc58c94f8cc9a4ec001197de971d0f12
FebryanShino <venty779@gmail.com>
2026-01-15 05:58:41 +0700

    fix: landing page UI and UX

--- resources/js/pages/anime/AnimeLandingPage.tsx
+++ resources/js/pages/anime/AnimeLandingPage.tsx
@@ -1,23 +1,42 @@
 import ContentWrapper from "@/components/app/ContentWrapper";
+import ResponsiveGridWrapper from "@/components/app/ResponsiveGridWrapper";
 import { Button } from "@/components/ui/button";
+import { useIsMobile } from "@/hooks/use-mobile";
+import AnimeLayout from "@/layouts/app/anime-layout";
+import { AnimeSeries } from "@/types/anime";
 import { Link } from "@inertiajs/react";
+import { PlusIcon } from "lucide-react";
 
-export default function AnimeLandingPage(props: {
-  animeList: { id: string; name: string; mal_id: string }[];
-}) {
-  console.log(props.animeList);
+export default function AnimeLandingPage(props: { animeList: AnimeSeries[] }) {
+  const isMobile = useIsMobile();
   return (
-    <ContentWrapper>
-      Anime list
-      <div>
-        {props.animeList.map((anime) => (
-          <Link href={`anime/id/${anime.mal_id}`}>
-            <Button>
-              <p>{anime.name}</p>
+    <AnimeLayout>
+      <ContentWrapper>
+        <div className="flex justify-between">
+          <h1 className="mb-10 text-4xl text-white font-bold">Anime List</h1>
+          <Link href={route("anime.form.upload")}>
+            <Button className="bg-white text-black hover:bg-blue-950 hover:text-white cursor-pointer">
+              Upload <PlusIcon />
             </Button>
           </Link>
-        ))}
-      </div>
-    </ContentWrapper>
+        </div>
+        <ResponsiveGridWrapper minSize={isMobile ? "8rem" : "12rem"}>
+          {props.animeList.map((anime) => (
+            <Link
+              href={route("anime.show", {
+                mal_id: anime.mal_id,
+              })}
+            >
+              <div
+                className="aspect-[17/24] bg-gray-900 rounded-lg bg-cover text-white flex items-center justify-center p-5 text-center font-bold"
+                style={{ backgroundImage: `url("${anime.cover}")` }}
+              >
+                {!anime.cover && anime.name}
+              </div>
+            </Link>
+          ))}
+        </ResponsiveGridWrapper>
+      </ContentWrapper>
+    </AnimeLayout>
   );
 }
